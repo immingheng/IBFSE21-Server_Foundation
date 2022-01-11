072c129ed2fd1ca010d50f182a74e4deded79eb6
immingheng <im.mingheng@gmail.com>
2022-01-11 14:27:36 +0800

    Day13

---
+++ workshop13/src/main/java/ibf2021/workshop3/model/Contact.java
@@ -0,0 +1,78 @@
+package ibf2021.workshop3.model;
+
+import java.io.Serializable;
+import java.security.SecureRandom;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Contact implements Serializable {
+    private String HexID;
+    private String name;
+    private String email;
+    private int phone_number;
+
+    // Constructor
+    public Contact() {
+        this.HexID = HexIDFormatting();
+    }
+
+    public Contact(String name, String email, int phone_number) {
+        this.name = name;
+        this.email = email;
+        this.phone_number = phone_number;
+    }
+
+    private synchronized String HexIDFormatting() {
+        /*
+         * String alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+         * // Want HexID to be 8 digit alphanumeric abcd1234
+         * for (int i = 0; i < 4; i++) {
+         * char alpha = alphabets.charAt((int) Math.round((Math.random() *
+         * alphabets.length())));
+         * HexID = HexID + alpha;
+         * }
+         * for (int j = 0; j < 4; j++) {
+         * Random randNum = new Random();
+         * int ranValue = randNum.nextInt(10);
+         * HexID = HexID + ranValue;
+         * }
+         * System.out.println(HexID);
+         */
+        SecureRandom sr = new SecureRandom();
+        int num = sr.nextInt(1 * 100000000);
+        String formattedHex = String.format("%08x", num);
+        return formattedHex;
+    }
+
+    // Setters
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhone_number(int phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    // Getters
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getPhone_number() {
+        return this.phone_number;
+    }
+
+    public String getHexID() {
+        return this.HexID;
+    }
+
+}
---
+++ workshop13/src/test/java/ibf2021/workshop3/Workshop13ApplicationTests.java
@@ -0,0 +1,13 @@
+package ibf2021.workshop3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workshop13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
