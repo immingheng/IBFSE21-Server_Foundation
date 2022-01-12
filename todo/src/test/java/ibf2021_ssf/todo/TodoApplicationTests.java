36dee25e246c0de59442ff8bc34682e8837d16ef
immingheng <im.mingheng@gmail.com>
2022-01-12 09:31:50 +0800

    Revision - TODO list App

---
+++ todo/src/test/java/ibf2021_ssf/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package ibf2021_ssf.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
